c13ddf6c17af845aadc16a14e1fc4eba8d9def99
Becky S <beckylouise.sloane@gmail.com>
2026-02-23 16:08:13 +0000

    Updating the location page locators

--- TestProjectUI/TestProjectUI/Locators/Location.cs
+++ TestProjectUI/TestProjectUI/Locators/Location.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,16 @@ namespace TestProjectUI.Locators
 {
     internal class Location
     {
+        // Section Ids
+        public By sectionBy = By.Id("location");
+
+        // Section Title
+        public By titleBy = By.XPath("//section[@id='location']//h2");
+
+        // Read Only Text Fields
+        public By addressBy = By.XPath("//section[@id='location']//h5[text()='Address']/following-sibling::p");
+        public By phoneBy = By.XPath("///section[@id='location']//h5[text()='Phone']/following-sibling::p"); 
+        public By emailBy = By.XPath("//section[@id='location']//h5[text()='Email']/following-sibling::p");
+
     }
 }
