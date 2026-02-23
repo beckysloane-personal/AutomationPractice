23cde22bbd6d585dad32c9f2eb376e5cffbb191f
Becky S <beckylouise.sloane@gmail.com>
2026-02-23 15:58:47 +0000

    Updating Home Page Locators

--- TestProjectUI/TestProjectUI/Locators/Home.cs
+++ TestProjectUI/TestProjectUI/Locators/Home.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,19 @@ namespace TestProjectUI.Locators
 {
     internal class Home
     {
+        // Section Title
+        public By titleBy = By.XPath("//h1");
+
+        // Buttons
+        public By bookNowBy = By.XPath("//a[text()='Book Now']");
+
+        // QuickLinks
+        public static By GenerateQuickLinkXPath(string pageName)
+        {
+            // Possible options for the quick links: [Rooms, Booking, Amenities, Location, Contact, Admin]
+            string quickLinkXPath = String.Format("//nav//a[text()='{0}']", pageName);
+            By quickLinkBy;
+            return quickLinkBy = By.XPath(quickLinkXPath);
+        }
     }
 }
