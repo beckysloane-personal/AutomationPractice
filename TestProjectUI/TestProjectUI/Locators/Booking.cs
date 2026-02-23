7b4f074da17dd6b40d36ddec7daa4875fac3b03e
Becky S <beckylouise.sloane@gmail.com>
2026-02-23 15:39:13 +0000

    Adding booking page locators

--- TestProjectUI/TestProjectUI/Locators/Booking.cs
+++ TestProjectUI/TestProjectUI/Locators/Booking.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TestProjectUI.Locators
 {
-    internal class Booking
+    public class Booking
     {
+        // Section Ids
+        public By sectionBy = By.Id("booking");
+
+        // Section Title
+        public By titleBy = By.XPath("//section[@id='booking']//h3");
+
+        // Date Inputs - Format: DD/MM/YYYY
+        public By checkInBy = By.XPath("//label[@for='checkin']/following-sibling::div//input");
+        public By checkOutBy = By.XPath("//label[@for='checkout']/following-sibling::div//input");
+
+        // Buttons
+        public By checkAvailabilityBy = By.XPath("//section[@id='booking']//button");
     }
 }
