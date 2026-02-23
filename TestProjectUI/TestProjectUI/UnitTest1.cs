7e327d5ca0ad99bca4ab4e6498e4b5b7bb549880
Becky S <beckylouise.sloane@gmail.com>
2026-02-23 15:05:22 +0000

    Setting up the project outline

---
+++ TestProjectUI/TestProjectUI/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProjectUI
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // https://automationintesting.online/
+        }
+    }
+}
