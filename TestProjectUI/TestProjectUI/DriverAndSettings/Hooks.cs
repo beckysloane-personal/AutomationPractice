ba164b5c58a33f924c829a81264c6f38f81c3ff2
Becky S <beckylouise.sloane@gmail.com>
2026-02-24 16:43:31 +0000

    Adding first feature file and setting up the hooks file

---
+++ TestProjectUI/TestProjectUI/DriverAndSettings/Hooks.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Reqnroll;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectUI.DriverAndSettings
+{
+    public class Hooks
+    {
+        public IWebDriver CreateDriver()
+        {
+            var chromeOptions = new ChromeOptions();
+
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+            return driver;
+        }
+
+        [Binding]
+        public class GlobalHooks
+        {
+            protected FeatureContext featureContext;
+            protected ScenarioContext scenarioContext;
+
+
+            public GlobalHooks(FeatureContext featureContext, ScenarioContext scenarioContext)
+            {
+                this.featureContext = featureContext;
+                this.scenarioContext = scenarioContext;
+            }
+        }
+
+        [BeforeScenario]
+        public void InitializeScenario(ScenarioContext scenarioContext)
+        {
+            var driver = CreateDriver();
+            scenarioContext["driver"] = driver;
+        }
+    }
+}
---
+++ TestProjectUI/TestProjectUI/Tests/BookingTests.feature
@@ -0,0 +1,9 @@
+﻿Feature: BookingTests
+
+UI tests using Selenium that cover the booking of a hotel room as a user
+
+@tag1
+Scenario: [scenario name]
+	Given [context]
+	When [action]
+	Then [outcome]
